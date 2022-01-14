5e8ab836f987fe017cf29279dd48743df98559b7
Szymon Ciepluch <szymon.ciepluch95@gmail.com>
2022-01-14 17:51:12 +0100

    Initial project structure

---
+++ src/main/java/pl/kielce/tu/sandworm/core/SandwormCoreApplication.java
@@ -0,0 +1,13 @@
+package pl.kielce.tu.sandworm.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandwormCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandwormCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kielce/tu/sandworm/core/SandwormCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kielce.tu.sandworm.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwormCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
