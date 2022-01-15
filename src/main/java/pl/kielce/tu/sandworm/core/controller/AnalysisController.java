5800721d31414b62c2af03bc20439bb2fa2e371e
Szymon Ciepluch <szymon.ciepluch95@gmail.com>
2022-01-15 14:21:11 +0100

    Analysis Controller implemented.

--- src/main/java/pl/kielce/tu/sandworm/core/controller/AnalysisController.java
+++ src/main/java/pl/kielce/tu/sandworm/core/controller/AnalysisController.java
@@ -1,4 +1,30 @@
 package pl.kielce.tu.sandworm.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.kielce.tu.sandworm.core.service.ProxyService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URISyntaxException;
+
+@RestController
 public class AnalysisController {
+
+    private final ProxyService proxyService;
+
+    @Autowired
+    public AnalysisController(ProxyService proxyService) {
+        this.proxyService = proxyService;
+    }
+
+    @RequestMapping("/**")
+    public ResponseEntity<String> analyzeRequest(@RequestBody String body, HttpMethod method, HttpServletRequest request,
+                                                 HttpServletResponse response) throws URISyntaxException {
+        return proxyService.proxyRequest(body, method, request, response);
+    }
 }
--- src/test/java/pl/kielce/tu/sandworm/core/SandwormCoreApplicationTests.java
+++ src/test/java/pl/kielce/tu/sandworm/core/SandwormCoreApplicationTests.java
@@ -1,13 +1,21 @@
 package pl.kielce.tu.sandworm.core;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.kielce.tu.sandworm.core.controller.AnalysisController;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SandwormCoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private AnalysisController analysisController;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(analysisController);
+    }
 
 }
