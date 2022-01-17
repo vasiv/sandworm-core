332eae14351328a1ec1ff890a6aa1bc0330db84c
Szymon Ciepluch <szymon.ciepluch95@gmail.com>
2022-01-17 14:49:21 +0100

    Analysis Service implemented, Analysis Result model class added.

--- src/main/java/pl/kielce/tu/sandworm/core/analysis/AnalysisResult.java
+++ src/main/java/pl/kielce/tu/sandworm/core/analysis/AnalysisResult.java
@@ -1,4 +1,16 @@
 package pl.kielce.tu.sandworm.core.analysis;
 
+import pl.kielce.tu.sandworm.core.model.Rule;
+
+import java.util.Set;
+
 public class AnalysisResult {
+
+    private RequestData request;
+    private Set<Rule> rulesTriggered;
+
+    public AnalysisResult(RequestData request, Set<Rule> rulesTriggered) {
+        this.request = request;
+        this.rulesTriggered = rulesTriggered;
+    }
 }
--- src/main/java/pl/kielce/tu/sandworm/core/service/StandardHttpAnalysisService.java
+++ src/main/java/pl/kielce/tu/sandworm/core/service/StandardHttpAnalysisService.java
@@ -3,16 +3,19 @@ package pl.kielce.tu.sandworm.core.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.kielce.tu.sandworm.core.analysis.AnalysisResult;
+import pl.kielce.tu.sandworm.core.analysis.RequestData;
+import pl.kielce.tu.sandworm.core.analysis.RuleHeaderMatcher;
 import pl.kielce.tu.sandworm.core.model.Rule;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StandardHttpAnalysisService implements HttpAnalysisService {
 
     Logger logger = LoggerFactory.getLogger(StandardHttpAnalysisService.class);
 
-    private Set<Rule> rules;
+    private final Set<Rule> rules;
 
     public StandardHttpAnalysisService(Set<Rule> rules) {
         this.rules = rules;
@@ -20,6 +23,20 @@ public class StandardHttpAnalysisService implements HttpAnalysisService {
 
     @Override
     public AnalysisResult analyze(HttpServletRequest request, String body) {
-        return null;
+        RequestData requestData = new RequestData(request, body);
+        Set<Rule> rulesTriggered = getTriggeredRules(requestData);
+        return new AnalysisResult(requestData, rulesTriggered);
     }
+
+    private Set<Rule> getTriggeredRules(RequestData requestData) {
+        RuleHeaderMatcher headerMatcher = new RuleHeaderMatcher(requestData);
+        return getMatchedRules(headerMatcher);
+    }
+
+    private Set<Rule> getMatchedRules(RuleHeaderMatcher headerMatcher) {
+        return rules.stream()
+                .filter(headerMatcher::doesHeaderMatch)
+                .collect(Collectors.toSet());
+    }
+
 }
